0b9a2aed6093206211e34e3b6d9ecddbc95cf128
shivanidhyani <shivanidhyani07@gmail.com>
2020-05-31 16:35:34 +0530

    url and views

--- webapp/urls.py
+++ webapp/urls.py
@@ -3,4 +3,9 @@ from . import views
 from django.conf.urls import include
 urlpatterns = [
 				path('', views.index, name='index'),
+				path('about_specso_technologies', views.about, name='about'),
+				path('blogs_of_specso_technologies', views.blog, name='blog'),
+				path('career_with_specso_technologies', views.career, name='career'),
+				path('projects_by_specso_technologies', views.ourwork, name='ourwork'),
+				path('contact_specso_technologies', views.contact, name='contact'),
 ]
--- webapp/views.py
+++ webapp/views.py
@@ -3,3 +3,18 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about_specso_technologies_software_development_company_in_india.html')
+
+def blog(request):
+    return render(request, 'blog_specso_technologies_software_development_company_in_india.html')
+
+def career(request):
+    return render(request, 'career_specso_technologies_software_development_company_in_india.html')
+
+def ourwork(request):
+    return render(request, 'our_work_specso_technologies_software_development_company_in_india.html')
+
+def contact(request):
+    return render (request, 'contact_specso_technologies_software_development_company_in_india.html')
