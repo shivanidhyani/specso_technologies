eb55ab95e3025fe6b37f5bdddf35c509670f41ce
shivanidhyani <shivanidhyani07@gmail.com>
2020-06-04 23:15:10 +0530

    updates

--- webapp/views.py
+++ webapp/views.py
@@ -14,7 +14,25 @@ def career(request):
     return render(request, 'career_specso_technologies_software_development_company_in_india.html')
 
 def ourwork(request):
-    return render(request, 'our_work_specso_technologies_software_development_company_in_india.html')
+    return render(request, 'work_of_specso_technologies_software_development_company_in_india.html')
 
 def contact(request):
     return render (request, 'contact_specso_technologies_software_development_company_in_india.html')
+
+def accountiee(request):
+    return render(request, 'accountiee_online_billing_software.html')
+
+def dkaksha(request):
+    return render(request, 'dkaksha_online_classes_software.html')
+
+def specplus(request):
+    return render(request, 'specplus_artificial_software.html')
+
+def stack(request):
+    return render(request, 'stack_erp_system.html')
+
+def social_connect(request):
+    return render(request, 'social_connect_automation_software.html')
+
+
+
