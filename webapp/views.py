a3a5e4f368c469e1c6047a78c0b6f3060aa2996d
shivanidhyani <shivanidhyani07@gmail.com>
2020-06-05 21:41:58 +0530

    updates

--- webapp/views.py
+++ webapp/views.py
@@ -7,18 +7,6 @@ def index(request):
 def about(request):
     return render(request, 'about_specso_technologies_software_development_company_in_india.html')
 
-def blog(request):
-    return render(request, 'blog_specso_technologies_software_development_company_in_india.html')
-
-def career(request):
-    return render(request, 'career_specso_technologies_software_development_company_in_india.html')
-
-def ourwork(request):
-    return render(request, 'work_of_specso_technologies_software_development_company_in_india.html')
-
-def contact(request):
-    return render (request, 'contact_specso_technologies_software_development_company_in_india.html')
-
 def accountiee(request):
     return render(request, 'accountiee_online_billing_software.html')
 
@@ -31,8 +19,47 @@ def specplus(request):
 def stack(request):
     return render(request, 'stack_erp_system.html')
 
-def social_connect(request):
-    return render(request, 'social_connect_automation_software.html')
+def web_development(request):
+    return render(request, 'web_development_services_of_specso_technologies.html')
 
+def e_commerce_development(request):
+    return render(request, 'e_commerce_development_services_of_specso_technologies.html')
 
+def software_development(request):
+    return render(request, 'software_development_services_of_specso_technologies.html')
 
+def mobile_application_development(request):
+    return render(request, 'mobile_application_development_services_of_specso_technologies.html')
+
+def erp_development(request):
+    return render(request, 'erp_development_services_of_specso_technologies.html')
+
+def database_management(request):
+    return render(request, 'database_management_services_of_specso_technologies.html')
+
+def search_engine_optimization(request):
+    return render(request, 'search_engine_optimization_services_of_specso_technologies.html')
+
+def social_media_marketing(request):
+    return render(request, 'social_media_marketing_services_of_specso_technologies.html')
+
+def graphic_designing(request):
+    return render(request, 'graphic_designing_services_of_specso_technologies.html')
+
+def web_hosting(request):
+    return render(request, 'web_hosting_services_of_specso_technologies.html')
+
+def blog(request):
+    return render(request, 'blog_specso_technologies_software_development_company_in_india.html')
+
+def career(request):
+    return render(request, 'career_specso_technologies_software_development_company_in_india.html')
+
+def ourwork(request):
+    return render(request, 'work_of_specso_technologies_software_development_company_in_india.html')
+
+def contact(request):
+    return render (request, 'contact_specso_technologies_software_development_company_in_india.html')
+
+def login(request):
+    return render (request, 'login_specso_technologies_software_development_company_in_india.html')
